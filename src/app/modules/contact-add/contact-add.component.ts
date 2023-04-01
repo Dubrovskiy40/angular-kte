bf6873473242ca972d20933e6ec4125fa03fdda8
Anatoliy Dubrovskiy <dubrovskiy.ru@mail.ru>
2023-04-01 10:56:53 +0300

    добавил форму для добавление нового пользователя

--- src/app/modules/contact-add/contact-add.component.ts
+++ src/app/modules/contact-add/contact-add.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {User, UserForm} from "../shared/types/types";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ResetForm} from "@ngxs/form-plugin";
+import {Store} from "@ngxs/store";
+import {AddUser} from "../contacts-list/store/users.actions";
+import {UsersState} from "../contacts-list/store/users.state";
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import {Router} from "@angular/router";
 
+@UntilDestroy()
 @Component({
   selector: 'app-contact-add',
   templateUrl: './contact-add.component.html',
   styleUrls: ['./contact-add.component.scss']
 })
-export class ContactAddComponent {
+export class ContactAddComponent implements OnDestroy{
+  public userForm: FormGroup<UserForm>;
+  private total!: number;
 
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly store: Store,
+    private readonly router: Router,
+    ) {
+    this.userForm = this.buildForm();
+    this.store.select(UsersState.total).pipe(untilDestroyed(this)).subscribe((total) => this.total = total);
+  }
+
+  public get formValue(): User {
+    const { ...value } = this.userForm.getRawValue();
+
+    return {
+      ...value,
+      id: this.total + 1,
+    };
+  }
+
+  public ngOnDestroy(): void {
+    this.resetUserForm();
+  }
+
+  public submitUserForm() {
+    if (!this.userForm.valid) {
+      return;
+    }
+    this.store.dispatch(new AddUser(this.formValue));
+
+    void this.router.navigate(['/', 'contacts']);
+  }
+  public clear() {
+    this.userForm.reset();
+  }
+
+  private resetUserForm() {
+    this.store.dispatch(
+      new ResetForm({
+        path: 'usersState.userForm',
+      }),
+    );
+  }
+
+  private buildForm(): FormGroup<UserForm> {
+    const form = this.formBuilder.group({
+      email: this.formBuilder.control('', {
+        validators: [Validators.email, Validators.required],
+        nonNullable: true,
+      }),
+      first_name: this.formBuilder.control('', {
+        validators: Validators.required,
+        nonNullable: true,
+      }),
+      last_name: this.formBuilder.control('', {
+        validators: Validators.required,
+        nonNullable: true,
+      }),
+      avatar: this.formBuilder.control('https://play-lh.googleusercontent.com/c_RFVsWfu89ctt7B6smzohv5UBVietoRji26alQNBlOJP6if4LzJAkL3Bw-2MxvuUKk', {
+        validators: Validators.required,
+        nonNullable: true,
+      }),
+    });
+
+    return form;
+  }
 }
--- src/app/modules/contact-add/contact-add.module.ts
+++ src/app/modules/contact-add/contact-add.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ContactAddComponent} from "./contact-add.component";
 import {ContactAddRoutingModule} from "./contact-add-routing.module";
-import {RouterModule} from "@angular/router";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {NgxsFormPluginModule} from "@ngxs/form-plugin";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {RequiredFieldDirective} from "../shared/custom-directives/required-field.directive";
 
 @NgModule({
   declarations: [ContactAddComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-    ContactAddRoutingModule,
-  ]
+    imports: [
+        CommonModule,
+        ContactAddRoutingModule,
+        NzFormModule,
+        NzInputModule,
+        ReactiveFormsModule,
+        NgxsFormPluginModule,
+        NzButtonModule,
+        RequiredFieldDirective,
+    ]
 })
 export class ContactAddModule { }
